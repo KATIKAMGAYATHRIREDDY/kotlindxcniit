3d5d3d75378fb772c27db49b5f3f43b8d1ca8a08
KATIKAMGAYATHRIREDDY <katikamgayathrireddy773@gmail.com>
2021-04-15 15:16:16 +0530

    Higher Order Ex

--- src/main/kotlin/functions/HigherOrder.kt
+++ src/main/kotlin/functions/HigherOrder.kt
@@ -7,6 +7,11 @@ fun add(a:Int,b:Int):Int{
     return a+b
 }
 fun sum(x:Int,y:Int)=x+y
+fun funWhichReturnsAnotherFun(): (Int)->Int{
+    return :: square
+}
+fun square(x:Int)=x*x
+
 fun mul(a:Int,b:Int):Int{
     return a*b
 }
@@ -15,6 +20,8 @@ fun calculate(x:Int,y:Int,operation: (Int,Int) ->Int ):Int{
     return operation(x,y)
 }
 fun main(){
+   val func= funWhichReturnsAnotherFun()
+    println(func(5))
     val resultSum=calculate(10,20,::sum)
     val resultMul=calculate(20,30,::mul)
     println(resultSum)
