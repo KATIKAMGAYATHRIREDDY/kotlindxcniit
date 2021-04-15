83851402268d90ab8b4ba7b690b838fb3326d2c0
KATIKAMGAYATHRIREDDY <katikamgayathrireddy773@gmail.com>
2021-04-15 11:30:35 +0530

    Higher order

---
+++ src/main/kotlin/functions/HigherOrder.kt
@@ -0,0 +1,22 @@
+package functions
+
+class HigherOrder {
+
+}
+fun add(a:Int,b:Int):Int{
+    return a+b
+}
+fun sum(x:Int,y:Int)=x+y
+fun mul(a:Int,b:Int):Int{
+    return a*b
+}
+
+fun calculate(x:Int,y:Int,operation: (Int,Int) ->Int ):Int{
+    return operation(x,y)
+}
+fun main(){
+    val resultSum=calculate(10,20,::sum)
+    val resultMul=calculate(20,30,::mul)
+    println(resultSum)
+    println(resultMul)
+}
